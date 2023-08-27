9e3eb6ec3e5e05cdaf74fe1fc17340fc88be22f6
LitavrinaAA <a.a.litavrina@google.com>
2023-08-27 16:12:36 +0300

    sum of digits in the entered number

---
+++ ex27/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число 
+//и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+int InputNum(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int SummDigitsInNum(int num)
+{
+    int sum = 0;
+
+    while (num != 0)
+    {
+        sum +=  num % 10;
+        num = num / 10;
+
+    }
+    return sum;
+}
+int num = InputNum("Input number: ");
+System.Console.WriteLine(SummDigitsInNum(num));
